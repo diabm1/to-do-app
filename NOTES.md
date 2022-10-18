363e30ed2c9bde3b6ccca99ab78f3cfc29cfe475
diabm1 <diabm1855@gmail.com>
2022-10-18 00:16:24 -0400

    trying to add code to stack task items on top of each other after item is submitted

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,14 @@
 I find that when I try to think up of a logic for a chunk of code. I start off fine but then as it becomes more complicated I get stuck.
 
 WHat I need to try to do is to try to write out the code by hand then resist the urge to code it out on vscode
+
+User Story
+As a user I want to be able to input a task item and have it show up as a list format underneath the add task bar
+
+As a user I want the ability to cross off a task item when it is completed.
+
+As a user I want the ability to delete a task item if it is no longer needed.
+
+As a user I want the task items to be there even after refreshing the page.
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,49 @@
 
 // The idea is to input a task item and then have it added to the list of items on screen
 
-const inputBox = document.getElementById("inputBox")
+const inputBox = document.getElementById("inputBox");
 
-console.log(inputBox.value)
+const submitBtn = document.getElementById("submitBtn");
 
-const submitBtn = document.getElementById("submitBtn")
-
-const taskItems = document.getElementById("taskItems")
-
+const taskItems = document.getElementById("taskItems");
 
 // function display(){
 //     inputBox.value
 // }
 
-function myFunction(){
-    taskItems.innerHTML = inputBox.value
-    // console.log(inputBox.value)
-    // `<h3>I'm clicked!</h3>`
+// A user can create a todo item by entering text into an input box and pressing a button
+function myFunction() {
+  // taskItems.innerHTML = `<h3>${inputBox.value}</h3>`
+
+  //Ok, now I have it return whatever I write in the task search box, now, how do I stack another task item on top of it?
+
+  //A list of created todo items are shown on the screen
+
+  //store it in local storage
+  //create an object out of the list items and whenever submit button is clicked it adds it to that list, maybe try an empty array?
+  // pushToArray()
+
+  let emptyArr = [];
+  let itemsPushed = emptyArr.push(inputBox.value);
+  for(let i = 0; i < emptyArr.length; i++){
+    
+  }
+
+  taskItems.innerHTML = `<h3>${itemsPushed}</h3>`
+
+  console.log(emptyArr)
+  console.log(itemsPushed)
+
+
 }
 
+function pushToArray() {
+  let emptyArr = [];
 
+  // console.log(myFunction())
+
+  //if myFunction is triggerd then push inputBox.value into the array
+  if (myFunction()) {
+    emptyArr.push(inputBox.value);
+  }
+}
