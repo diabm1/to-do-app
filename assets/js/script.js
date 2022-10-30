ed1e591b4d45e926fa70977662d7bbefea18df5e
diabm1 <diabm1855@gmail.com>
2022-10-30 16:43:13 -0400

    made few more changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,11 @@ const inputBox = document.getElementById("inputBox");
 const submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click", function handleClick(event) {
-  // if you are submitting a form (prevents page reload)
   event.preventDefault();
-
-  //appendChild("divContainingTasks")
-  //appendChild("taskItems")
-  //appendChild
-
-  // const taskItems = document.getElementById("taskItems");
   const divContainingTasks = document.getElementById("divContainingTasks");
 
   const p = document.createElement("p");
   p.id = "taskItems";
-  // span.id = "sideBySide"
-  // const sideBySide = document.getElementById("sideBySide")
-  // const sideBySide = document.getElementById("taskItems")
   divContainingTasks.appendChild(p);
 
   const inputCheckBox = document.createElement("input");
@@ -39,28 +29,26 @@ submitBtn.addEventListener("click", function handleClick(event) {
 
   delBtn.addEventListener("click", function () {
     if (inputCheckBox.checked === true) {
+      //delete item
       inputCheckBox.style.display = "none";
       toDoEl.style.display = "none";
       delBtn.style.display = "none";
     } else if (inputCheckBox.checked === false) {
+      //do not delete item
       inputCheckBox.style.display = "inline-block";
       toDoEl.style.display = "inline-block";
       delBtn.style.display = "inline-block";
     }
   });
-
-  // let elementClicked = false;
-
+  
   //if clicked then strike through
   inputCheckBox.addEventListener("click", function () {
     console.log(inputCheckBox.checked);
 
     if (inputCheckBox.checked === true) {
       toDoEl.style.textDecoration = "line-through";
-      //delete item
     } else if (inputCheckBox.checked === false) {
       toDoEl.style.textDecoration = "none";
-      //do not delete item
     }
   });
 
@@ -68,45 +56,4 @@ submitBtn.addEventListener("click", function handleClick(event) {
   inputBox.value = "";
 });
 
-// function display(){
-//     inputBox.value
-// }
 
-// A user can create a todo item by entering text into an input box and pressing a button
-// function myFunction(e) {
-//   // taskItems.innerHTML = `<h3>${inputBox.value}</h3>`
-
-//   //Ok, now I have it return whatever I write in the task search box, now, how do I stack another task item on top of it?
-
-//   //A list of created todo items are shown on the screen
-
-//   //store it in local storage
-//   //create an object out of the list items and whenever submit button is clicked it adds it to that list, maybe try an empty array?
-//   // pushToArray()
-
-//   // let emptyArr = [];
-
-//   // for (let i = 0; i < 5; i++) {
-//   //   emptyArr.push(inputBox.value);
-//   //   taskItems.innerHTML = `<h3>${inputBox.value}</h3>`;
-//   // }
-//   // 👇️ if you are submitting a form (prevents page reload)
-//   e.preventDefault();
-
-//   // Send value to server
-//   console.log(inputBox.value);
-
-//   // 👇️ clear input field
-//   inputBox.value = "";
-// }
-
-// function pushToArray() {
-//   let emptyArr = [];
-
-//   // console.log(myFunction())
-
-//   //if myFunction is triggerd then push inputBox.value into the array
-//   if (myFunction()) {
-//     emptyArr.push(inputBox.value);
-//   }
-// }
